e6579d864df8377fa5438bbba8acb966357a3d17
Stefania <tudorica.stefania@gmail.com>
2022-06-29 20:32:05 +0300

    exercise 5 final

---
+++ src/main/java/com/example/ex5/AppEx5.java
@@ -0,0 +1,15 @@
+package com.example.ex5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEx5 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEx5.class, args);
+	}
+
+
+	}
+
