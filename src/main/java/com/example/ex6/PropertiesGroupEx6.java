6793d4d40ef523b52e7bd36b2a83421025b9f6b5
DESKTOP-G1OVDRU\marin <marinioana23@yahoo.com>
2022-06-30 20:38:18 +0300

    ex6 validated

--- src/main/java/com/example/ex6/PropertiesGroupEx6.java
+++ src/main/java/com/example/ex6/PropertiesGroupEx6.java
@@ -3,13 +3,16 @@ package com.example.ex6;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
+import javax.validation.constraints.*;
 import java.util.Map;
 
-
+@Validated
 @Component // or //@EnableConfigurationProperties(PropertiesGroup.class)
 @ConfigurationProperties(prefix = "pl.sdacademy.zad6")
 @Data
@@ -17,12 +20,32 @@ import java.util.Map;
 @AllArgsConstructor
 public class PropertiesGroupEx6 {
 
+    @Email
     private String email;
+
+    @NotNull
+    @Length(min = 3, max = 20)
     private String firstName;
+
+    @NotNull
+    @Length(min = 3, max = 20)
     private String lastName;
+
+
     private String address;
+    @AssertTrue
+    public boolean isCorrect(){
+        return address!=null && address.split(" ").length == 2;
+    }
+
+    @NotNull
+    @Min(18)
     private Integer age;
+
+    @NotEmpty
     private String[] values;
+
+    @NotEmpty
     private Map<String, String> customAttributes;
 }
 
